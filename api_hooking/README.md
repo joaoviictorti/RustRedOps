61ef7d05d6b4495f042623e2342d9d35d65cb5c4
João <joaovictorti08@gmail.com>
2023-12-08 19:59:08 -0300

    feat: add API Hooking

---
+++ api_hooking/README.md
@@ -0,0 +1,37 @@
+# API Hooking 🦀
+
+<p align="left">
+	<a href="https://www.rust-lang.org/"><img src="https://img.shields.io/badge/made%20with-Rust-red"></a>
+	<a href="#"><img src="https://img.shields.io/badge/platform-windows-blueviolet"></a>
+</p>
+
+- [Overview](#overview)
+- [Running](#running)
+- [Usage](#usage)
+
+# Overview
+API hooking in Windows, calls to these APIs are intercepted, allowing them to be monitored, modified or redirected. This is done by inserting intermediate code (the "hook") between the function call and its actual execution. When an application calls an API function, the hook is triggered first, allowing the operation to be manipulated.
+
+# Running
+
+Performing the API Hooking technique in MessageBoxA:
+![](/img//hook_enabled.png)
+
+Checking the debugger for the change:
+![](/img//debugger_enabled.png)
+
+Eliminating the API hook:
+![](/img/hook_disabled.png)
+
+Checking the debugger when disabling the hook:
+![](/img/debugger_disabled.png)
+
+# Usage 
+
+You can run with cargo run or the compiled binary directly:
+```sh
+cargo run
+```
+```sh
+target/release/api_hooking.exe
+```
---
+++ api_hooking/src/main.rs
@@ -0,0 +1,133 @@
+use std::ffi::{c_char, CStr};
+use std::{mem::size_of, os::raw::c_void, ptr::copy};
+use windows::{
+    core::{s, w},
+    Win32::{
+        Foundation::HWND,
+        System::LibraryLoader::{GetProcAddress, LoadLibraryA},
+        System::Memory::{VirtualProtect, PAGE_EXECUTE_READWRITE, PAGE_PROTECTION_FLAGS},
+        UI::WindowsAndMessaging::{MessageBoxA, MessageBoxW, MESSAGEBOX_STYLE},
+        UI::WindowsAndMessaging::{MB_OK, MESSAGEBOX_RESULT},
+    },
+};
+
+extern "system" fn my_message_box_a(
+    hwnd: HWND,
+    lp_text: *const c_char,
+    lp_caption: *const c_char,
+    u_type: MESSAGEBOX_STYLE,
+) -> MESSAGEBOX_RESULT {
+    let c_str_text = unsafe { CStr::from_ptr(lp_text) };
+    let text = c_str_text.to_string_lossy();
+
+    let c_str_caption = unsafe { CStr::from_ptr(lp_caption) };
+    let caption = c_str_caption.to_string_lossy();
+
+    println!("[+] Parameters sent by the original function:");
+    println!("\t - lp_text    : {}", text);
+    println!("\t - lp_caption : {}", caption);
+
+    unsafe { MessageBoxW(hwnd, w!("HOOK"), w!("ENABLED!"), u_type) }
+}
+
+struct Hook {
+    #[cfg(target_arch = "x86_64")]
+    bytes_original: [u8; 13],
+    #[cfg(target_arch = "x86")]
+    bytes_original: [u8; 7],
+    function_run: *mut c_void,
+    function_hook: *mut c_void,
+}
+
+impl Hook {
+    fn new(function_run: *mut c_void, function_hook: *mut c_void) -> Self {
+        Hook {
+            #[cfg(target_arch = "x86_64")]
+            bytes_original: [0; 13],
+            #[cfg(target_arch = "x86")]
+            bytes_original: [0; 7],
+            function_run,
+            function_hook,
+        }
+    }
+
+    fn initialize(&mut self, trampoline: &[u8]) -> bool {
+        unsafe {
+            copy(
+                self.function_hook,
+                self.bytes_original.as_mut_ptr() as *mut c_void,
+                trampoline.len(),
+            );
+
+            let mut old_protect = PAGE_PROTECTION_FLAGS(0);
+            let result = VirtualProtect(
+                self.function_hook,
+                trampoline.len(),
+                PAGE_EXECUTE_READWRITE,
+                &mut old_protect,
+            );
+            if result.is_err() {
+                println!("[!] VirtualProtect Failed With Error {:?}", result.err());
+                return false;
+            }
+        }
+        true
+    }
+
+    fn install_hook(&self, trampoline: &[u8]) {
+        unsafe {
+            copy(
+                &self.function_run as *const _ as *const c_void,
+                trampoline[2..].as_ptr() as *mut c_void,
+                size_of::<*mut c_void>(),
+            );
+
+            copy(
+                trampoline.as_ptr() as *const c_void,
+                self.function_hook,
+                trampoline.len(),
+            );
+        }
+    }
+}
+
+fn main() {
+    #[cfg(target_arch = "x86_64")]
+    let mut trampoline: [u8; 13] = [
+        0x49, 0xBA, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // mov r10, function
+        0x41, 0xFF, 0xE2, // jmp r10
+    ];
+
+    #[cfg(target_arch = "x86")]
+    let mut trampoline: [u8; 7] = [
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // mov eax, function
+        0xFF, 0xE0, // jmp eax
+    ];
+
+    let hmodule = unsafe { LoadLibraryA(s!("user32.dll")).unwrap() };
+    let func = unsafe { GetProcAddress(hmodule, s!("MessageBoxA")).unwrap() };
+
+    let mut hook = Hook::new(my_message_box_a as *mut c_void, func as *mut c_void);
+
+    if hook.initialize(&mut trampoline) {
+        hook.install_hook(&trampoline);
+    } else {
+        println!("[!] Failed to Apply Hook!");
+        return;
+    }
+
+    unsafe {
+        MessageBoxA(HWND(0), s!("Test Message"), s!("Test"), MB_OK);
+
+        println!("[+] Hook disabled");
+        copy(
+            hook.bytes_original.as_ptr(),
+            hook.function_hook as *mut u8,
+            trampoline.len(),
+        );
+
+        MessageBoxA(HWND(0), s!("Test Message"), s!("Test"), MB_OK);
+    }
+
+    println!("[+] Finish");
+}
