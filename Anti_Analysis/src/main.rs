74d18956eb49dfcb719ca04f72fd5cab13c674d5
João <joaovictorti08@gmail.com>
2024-06-14 09:50:27 -0300

    refactor: update code Anti-Analysis

--- Anti_Analysis/src/main.rs
+++ Anti_Analysis/src/main.rs
@@ -1,11 +1,14 @@
+#![allow(unused_must_use)]
+
 use sysinfo::System;
 use windows::core::{s, PSTR};
-use windows::Win32::System::SystemInformation::{
-    GetSystemInfo, GlobalMemoryStatusEx, MEMORYSTATUSEX, SYSTEM_INFO,
-};
-
-use windows::Win32::System::Registry::{
-    RegCloseKey, RegOpenKeyExA, RegQueryInfoKeyA, HKEY, HKEY_LOCAL_MACHINE, KEY_READ,
+use windows::Win32::System::{
+    SystemInformation::{
+        GetSystemInfo, GlobalMemoryStatusEx, MEMORYSTATUSEX, SYSTEM_INFO
+    },
+    Registry::{
+        RegCloseKey, RegOpenKeyExA, RegQueryInfoKeyA, HKEY, HKEY_LOCAL_MACHINE, KEY_READ
+    },
 };
 
 fn main() {
@@ -15,11 +18,9 @@ fn main() {
     verify_processes();
 }
 
-/*
-
-    Function that performs a check on the CPU to find out how many processors the computer contains.
-
-*/
+///
+/// Function that performs a check on the CPU to find out how many processors the computer contains.
+///
 fn verify_cpu() {
     let mut info: SYSTEM_INFO = SYSTEM_INFO::default();
 
@@ -32,17 +33,15 @@ fn verify_cpu() {
     }
 }
 
-/*
-
- Function that performs a check of the current physical memory in bytes and in it we are checking if it is greater than or equal to two gigabytes in bytes.
-
-*/
+///
+/// Function that performs a check of the current physical memory in bytes and in it we are checking if it is greater than or equal to two gigabytes in bytes.
+///
 fn verify_ram() {
     let mut info: MEMORYSTATUSEX = MEMORYSTATUSEX::default();
     info.dwLength = std::mem::size_of::<MEMORYSTATUSEX>() as u32;
 
     unsafe {
-        let _ = GlobalMemoryStatusEx(&mut info).expect("GlobalMemoryStatusEx Failed");
+        GlobalMemoryStatusEx(&mut info).expect("GlobalMemoryStatusEx Failed");
 
         if info.ullTotalPhys <= 2 * 1073741824 {
             println!("[*] Possibly a virtualised environment")
@@ -50,14 +49,11 @@ fn verify_ram() {
     }
 }
 
-/*
-
-  The SYSTEM\ControlSet001\Enum\USBSTOR directory in the Windows Registry is a specific location where the operating system
-  stores information about USB storage devices that have been connected to the computer.
-
-  Possibly if the computer didn't have 2 usb mounted, it may be in a virtualised environment
-
-*/
+///
+/// The SYSTEM\ControlSet001\Enum\USBSTOR directory in the Windows Registry is a specific location where the operating system
+/// stores information about USB storage devices that have been connected to the computer.
+/// 
+/// Possibly if the computer didn't have 2 usb mounted, it may be in a virtualised environment
 fn verify_usb() {
     let mut h_key: HKEY = HKEY::default();
     let mut usb_number: u32 = 0;
@@ -103,15 +99,13 @@ fn verify_usb() {
             return;
         }
 
-        let _ = RegCloseKey(h_key);
+        RegCloseKey(h_key);
     }
 }
 
-/*
-
-  Check if the environment can be sandboxed through the number of processes running
-
-  */
+////
+/// Check if the environment can be sandboxed through the number of processes running
+/// 
 fn verify_processes() {
     let mut system = System::new_all();
     system.refresh_all();
