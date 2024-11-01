fbc63539aca7f3ac6a74e12b9f7844bd39301e73
joaoviictorti <joaovictorti08@gmail.com>
2024-11-01 15:59:12 -0300

    fix: Solving PEB recovery errors

--- Anti_Debug/src/main.rs
+++ Anti_Debug/src/main.rs
@@ -1,17 +1,21 @@
-use std::arch::asm;
 use sysinfo::System;
-use windows::Win32::System::Diagnostics::Debug::{
-    GetThreadContext, IsDebuggerPresent, CONTEXT, CONTEXT_DEBUG_REGISTERS_AMD64
+use windows::core::Error;
+use windows::Win32::System::{
+    Kernel::NT_TIB,
+    Threading::{GetCurrentThread, PEB, TEB},
+    Diagnostics::Debug::{
+        GetThreadContext, IsDebuggerPresent, 
+        CONTEXT, CONTEXT_DEBUG_REGISTERS_AMD64
+    }
 };
-use windows::Win32::System::Threading::{GetCurrentThread, PEB, TEB};
-use windows::Win32::System::Kernel::NT_TIB;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     is_debugger_present();
     is_debugger_peb();
     process_list();
-    breakpoint_hardware();
-    std::thread::sleep(std::time::Duration::from_secs(20000));
+    breakpoint_hardware()?;
+
+    Ok(())
 }
 
 fn is_debugger_present() {
@@ -54,57 +58,36 @@ fn process_list() {
     }
 }
 
-fn breakpoint_hardware() {
-    let mut ctx = CONTEXT::default();
+fn breakpoint_hardware() -> Result<(), Error> {
+    let mut ctx = CONTEXT {
+        ContextFlags: CONTEXT_DEBUG_REGISTERS_AMD64,
+        ..Default::default()
+    };
 
-    ctx.ContextFlags = CONTEXT_DEBUG_REGISTERS_AMD64;
-
-    unsafe { GetThreadContext(GetCurrentThread(), &mut ctx).unwrap_or_else(|e| {
-        println!("[!] GetThreadContext Failed With Error: {e}");
-    }) };
+    unsafe { GetThreadContext(GetCurrentThread(), &mut ctx) }?;
 
     if ctx.Dr0 != 0 || ctx.Dr1 != 0 || ctx.Dr2 != 0 || ctx.Dr3 != 0 {
         println!("[!] Debugger Detected! [4]");
     }
+
+    Ok(())
 }
 
-// Function to recover PEB
 unsafe fn get_peb() -> *mut PEB {
     let teb_offset = ntapi::FIELD_OFFSET!(NT_TIB, Self_) as u32;
 
     #[cfg(target_arch = "x86_64")]
     {
+        use ntapi::winapi_local::um::winnt::__readgsqword;
+
         let teb = __readgsqword(teb_offset) as *mut TEB;
         return (*teb).ProcessEnvironmentBlock;
     }
 
     #[cfg(target_arch = "x86")]
     {
+        use ntapi::winapi_local::um::winnt::__readfsdword;
         let teb = __readfsdword(teb_offset) as *mut TEB;
         return (*teb).ProcessEnvironmentBlock;
     }
-}
-
-#[cfg(target_arch = "x86_64")]
-unsafe fn __readgsqword(offset: u32) -> u64 {
-    let output: u64;
-    asm!(
-        "mov {}, gs:[{:e}]",
-        lateout(reg) output,
-        in(reg) offset,
-        options(nostack, pure, readonly),
-    );
-    output
-}
-
-#[cfg(target_arch = "x86")]
-unsafe fn __readfsdword(offset: u32) -> u32 {
-    let output: u32;
-    asm!(
-        "mov {:e}, fs:[{:e}]",
-        lateout(reg) output,
-        in(reg) offset,
-        options(nostack, pure, readonly),
-    );
-    output
 }
