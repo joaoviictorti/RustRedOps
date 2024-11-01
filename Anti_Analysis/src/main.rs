c672e4f5392975546d93224e56e472fa65992abe
joaoviictorti <joaovictorti08@gmail.com>
2024-11-01 15:54:09 -0300

    style: Improving code style for better readability

--- Anti_Analysis/src/main.rs
+++ Anti_Analysis/src/main.rs
@@ -1,28 +1,32 @@
-#![allow(unused_must_use)]
+// #![allow(unused_must_use)]
 
 use sysinfo::System;
-use windows::core::{s, PSTR};
+use windows::core::{s, Error, PSTR};
 use windows::Win32::System::{
     SystemInformation::{
-        GetSystemInfo, GlobalMemoryStatusEx, MEMORYSTATUSEX, SYSTEM_INFO
+        GetSystemInfo, 
+        GlobalMemoryStatusEx, 
+        MEMORYSTATUSEX, SYSTEM_INFO
     },
     Registry::{
-        RegCloseKey, RegOpenKeyExA, RegQueryInfoKeyA, HKEY, HKEY_LOCAL_MACHINE, KEY_READ
+        RegCloseKey, RegOpenKeyExA, 
+        RegQueryInfoKeyA, HKEY, HKEY_LOCAL_MACHINE, 
+        KEY_READ
     },
 };
 
-fn main() {
-    verify_usb();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    verify_usb()?;
     verify_ram();
     verify_cpu();
     verify_processes();
+
+    Ok(())
 }
 
-///
 /// Function that performs a check on the CPU to find out how many processors the computer contains.
-///
 fn verify_cpu() {
-    let mut info: SYSTEM_INFO = SYSTEM_INFO::default();
+    let mut info = SYSTEM_INFO::default();
 
     unsafe {
         GetSystemInfo(&mut info);
@@ -33,11 +37,10 @@ fn verify_cpu() {
     }
 }
 
-///
-/// Function that performs a check of the current physical memory in bytes and in it we are checking if it is greater than or equal to two gigabytes in bytes.
-///
+/// Function that performs a check of the current physical memory in bytes and 
+/// in it we are checking if it is greater than or equal to two gigabytes in bytes.
 fn verify_ram() {
-    let mut info: MEMORYSTATUSEX = MEMORYSTATUSEX::default();
+    let mut info = MEMORYSTATUSEX::default();
     info.dwLength = std::mem::size_of::<MEMORYSTATUSEX>() as u32;
 
     unsafe {
@@ -49,32 +52,26 @@ fn verify_ram() {
     }
 }
 
-///
 /// The SYSTEM\ControlSet001\Enum\USBSTOR directory in the Windows Registry is a specific location where the operating system
 /// stores information about USB storage devices that have been connected to the computer.
 /// 
 /// Possibly if the computer didn't have 2 usb mounted, it may be in a virtualised environment
-fn verify_usb() {
-    let mut h_key: HKEY = HKEY::default();
-    let mut usb_number: u32 = 0;
+fn verify_usb() -> Result<(), Error> {
+    let mut h_key = HKEY::default();
+    let mut usb_number = 0;
     let mut class_name_buffer = [0u8; 256];
     let mut class_name_length = class_name_buffer.len() as u32;
 
     unsafe {
-        let status = RegOpenKeyExA(
+        RegOpenKeyExA(
             HKEY_LOCAL_MACHINE,
             s!("SYSTEM\\ControlSet001\\Enum\\USBSTOR"),
             0,
             KEY_READ,
             &mut h_key,
-        );
+        )?;
 
-        if status.is_err() {
-            println!("RegOpenKeyExA Failed");
-            return;
-        }
-
-        let status = RegQueryInfoKeyA(
+        RegQueryInfoKeyA(
             h_key,
             PSTR(class_name_buffer.as_mut_ptr()),
             Some(&mut class_name_length),
@@ -87,25 +84,20 @@ fn verify_usb() {
             None,
             None,
             None,
-        );
-
-        if status.is_err() {
-            println!("RegQueryInfoKeyA Failed");
-            return;
-        }
+        )?;
 
         if usb_number < 2 {
             println!("[*] Possibly a virtualised environment");
-            return;
+            return Ok(());
         }
 
-        RegCloseKey(h_key);
+        RegCloseKey(h_key)?;
     }
+
+    Ok(())
 }
 
-////
 /// Check if the environment can be sandboxed through the number of processes running
-/// 
 fn verify_processes() {
     let mut system = System::new_all();
     system.refresh_all();
