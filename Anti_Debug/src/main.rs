12c4d6f77b457adb6cf0fac9825de6c7b4924e74
joaoviictorti <joaovictorti08@gmail.com>
2025-04-28 22:01:15 -0300

    Remove file

--- Anti_Debug/src/main.rs
+++ Anti_Debug/src/main.rs
@@ -1,8 +1,7 @@
 use sysinfo::System;
 use windows::core::Error;
 use windows::Win32::System::{
-    Kernel::NT_TIB,
-    Threading::{GetCurrentThread, PEB, TEB},
+    Threading::{GetCurrentThread, PEB},
     Diagnostics::Debug::{
         GetThreadContext, IsDebuggerPresent, 
         CONTEXT, CONTEXT_DEBUG_REGISTERS_AMD64
@@ -37,7 +36,7 @@ fn is_debugger_present() {
 /// If the flag is set, it logs a message to the console.
 fn is_debugger_peb() {
     unsafe {
-        let peb = get_peb();
+        let peb = NtCurrentPeb();
         if (*peb).BeingDebugged == 1 {
             println!("[!] Debugger Detected! [{}]", line!());
         }
@@ -80,6 +79,7 @@ fn process_list() {
 /// If any debug register is non-zero, it logs a message to the console.
 ///
 /// # Errors
+/// 
 /// Returns an error if `GetThreadContext` fails.
 fn breakpoint_hardware() -> Result<(), Error> {
     let mut ctx = CONTEXT {
@@ -96,22 +96,63 @@ fn breakpoint_hardware() -> Result<(), Error> {
     Ok(())
 }
 
-/// Retrieves the `PEB` pointer for the current process.
-unsafe fn get_peb() -> *mut PEB {
-    let teb_offset = ntapi::FIELD_OFFSET!(NT_TIB, Self_) as u32;
+/// Retrieves a pointer to the Process Environment Block (PEB) of the current process.
+/// 
+/// # Returns
+/// 
+/// * Pointer to the PEB structure.
+#[inline(always)]
+#[allow(non_snake_case)]
+pub fn NtCurrentPeb() -> *const PEB {
+    unsafe {
+        #[cfg(target_arch = "x86_64")]
+        return __readgsqword(0x60) as *const PEB;
 
-    #[cfg(target_arch = "x86_64")]
-    {
-        use ntapi::winapi_local::um::winnt::__readgsqword;
-
-        let teb = __readgsqword(teb_offset) as *mut TEB;
-        return (*teb).ProcessEnvironmentBlock;
+        #[cfg(target_arch = "x86")]
+        return __readfsdword(0x30) as *const PEB;
     }
+}
 
-    #[cfg(target_arch = "x86")]
-    {
-        use ntapi::winapi_local::um::winnt::__readfsdword;
-        let teb = __readfsdword(teb_offset) as *mut TEB;
-        return (*teb).ProcessEnvironmentBlock;
-    }
+/// Reads a `u64` value from the GS segment at the specified offset.
+/// 
+/// # Arguments
+/// 
+/// * `offset` - The offset from the GS base where the value is located.
+/// 
+/// # Returns
+/// 
+/// * The value read from the GS segment.
+#[inline(always)]
+#[cfg(target_arch = "x86_64")]
+pub unsafe fn __readgsqword(offset: u64) -> u64 {
+    let out: u64;
+    core::arch::asm!(
+        "mov {}, gs:[{:e}]",
+        lateout(reg) out,
+        in(reg) offset,
+        options(nostack, pure, readonly),
+    );
+    out
+}
+
+/// Reads a `u32` value from the FS segment at the specified offset.
+/// 
+/// # Arguments
+/// 
+/// * `offset` - The offset from the FS base where the value is located.
+/// 
+/// # Returns
+/// 
+/// * The value read from the FS segment.
+#[inline(always)]
+#[cfg(target_arch = "x86")]
+pub unsafe fn __readfsdword(offset: u32) -> u32 {
+    let out: u32;
+    core::arch::asm!(
+        "mov {:e}, fs:[{:e}]",
+        lateout(reg) out,
+        in(reg) offset,
+        options(nostack, pure, readonly),
+    );
+    out
 }
