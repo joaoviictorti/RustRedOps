fa446551f49af7c60c3e40a9e2e58ca0cc3a0aab
joaoviictorti <joaovictorti08@gmail.com>
2025-04-28 23:01:50 -0300

    docs: fix issues in code examples

--- Payload_Execution_Control/README.md
+++ Payload_Execution_Control/README.md
@@ -1,22 +1,10 @@
 # Payload Execution Control 🦀
 
-<p align="left">
-	<a href="https://www.rust-lang.org/"><img src="https://img.shields.io/badge/made%20with-Rust-red"></a>
-	<a href="#"><img src="https://img.shields.io/badge/platform-windows-blueviolet"></a>
-</p>
-
-- [Overview](#overview)
-- [Usage](#usage)
-
-# Overview
 Mutexes, events and semaphores are fundamental synchronization mechanisms in operating system programming, especially in the context of payload execution control in malware development. These mechanisms allow processes to coordinate their actions efficiently, ensuring that shared resources are accessed safely and that race conditions are avoided.
 
-# Usage 
+## Usage 
 
 You can run with cargo run or the compiled binary directly:
 ```sh
-cargo run
-```
-```sh
-target/release/execution_control.exe
+cargo run --release
 ```
--- Payload_Execution_Control/src/main.rs
+++ Payload_Execution_Control/src/main.rs
@@ -1,26 +1,34 @@
-use windows::Win32::Foundation::GetLastError;
+use windows::Win32::Foundation::{GetLastError, HANDLE};
 use windows::Win32::System::Threading::{CreateEventA, CreateMutexA, CreateSemaphoreA};
-use windows::core::s;
+use windows::core::{s, Result};
 
-fn main() {
+fn main() -> Result<()> {
     unsafe {
-        events();
+        events()?;
+        
+        // If the event already exists (`GetLastError().is_err()`), it prints a custom message
+        // indicating that another instance of the malware might already be running.
         if GetLastError().is_err() {
             println!("[*] MALWARE RUNNING");
         }
     }
+
+    Ok(())
 }
 
+/// Creates a named mutex.
 #[allow(dead_code)]
-unsafe fn mutex() { 
-    let _ = CreateMutexA(None, false, s!("MalwareA"));
+fn mutex() -> Result<HANDLE> { 
+    unsafe { CreateMutexA(None, false, s!("MalwareA")) }
 }
 
+/// Creates a named semaphore.
 #[allow(dead_code)]
-unsafe fn semaphore() {
-    let _ = CreateSemaphoreA(None, 10, 10, s!("MalwareA"));
+fn semaphore() -> Result<HANDLE> {
+    unsafe { CreateSemaphoreA(None, 10, 10, s!("MalwareA")) } 
 }
 
-unsafe fn events() {
-    let _ = CreateEventA(None, false, false, s!("MalwareA"));
+/// Creates a named event object.
+fn events() -> Result<HANDLE> {
+    unsafe { CreateEventA(None, false, false, s!("MalwareA")) }
 }
