37e1a41236d2a68ff7c93a6f8f5db07c13853ec7
joaoviictorti <joaovictorti08@gmail.com>
2025-04-28 23:24:31 -0300

    docs: fix issues in code examples

--- PPID_Spoofing/README.md
+++ PPID_Spoofing/README.md
@@ -1,24 +1,10 @@
 # PPID Spoofing 🦀
 
-<p align="left">
-	<a href="https://www.rust-lang.org/"><img src="https://img.shields.io/badge/made%20with-Rust-red"></a>
-	<a href="#"><img src="https://img.shields.io/badge/platform-windows-blueviolet"></a>
-</p>
-
-- [Overview](#overview)
-- [Usage](#usage)
-
-# Overview
 This project focuses on PPID Spoofing, or "Parent Process ID Spoofing", an advanced security evasion technique often used by malware to hide its malicious activities. This technique involves modifying the Parent Process Identifier (PPID) of a malicious process to make it appear that it was started by a trusted or legitimate process.
 
-# Usage 
+## Usage 
 
 You can run with cargo run or the compiled binary directly:
 ```sh
-cargo run
+cargo run --release
 ```
-```sh
-target/release/ppid_spoofing.exe
-```
-
-
--- PPID_Spoofing/src/main.rs
+++ PPID_Spoofing/src/main.rs
@@ -1,6 +1,6 @@
 use std::{ffi::c_void, mem::size_of, ptr::null_mut};
 use windows::{
-    core::PSTR,
+    core::{Result, PSTR},
     Win32::{
         Foundation::HANDLE,
         System::{
@@ -15,28 +15,32 @@ use windows::{
     },
 };
 
-fn main() {
-    let mut startup_info = STARTUPINFOEXA::default();
-    let mut process_info = PROCESS_INFORMATION::default();
-    startup_info.StartupInfo.cb = size_of::<STARTUPINFOEXA>() as u32;
+fn main() -> Result<()> {
+    let mut si = STARTUPINFOEXA::default();
+    let mut pi = PROCESS_INFORMATION::default();
+    si.StartupInfo.cb = size_of::<STARTUPINFOEXA>() as u32;
+
     unsafe {
-        let h_parent_process = OpenProcess(PROCESS_ALL_ACCESS, false, 10404).unwrap_or_else(|err| panic!("Error opening parent process: {}", err)); // PPID
-        let mut attr_size: usize = 0;
+        // Open handle to the parent process by PID
+        let h_parent_process = OpenProcess(PROCESS_ALL_ACCESS, false, 35372)?;
+        
+        // Initialize the attribute list
+        let mut attr_size = 0;
         let _ = InitializeProcThreadAttributeList(
             LPPROC_THREAD_ATTRIBUTE_LIST(null_mut()),
             1,
             0,
             &mut attr_size,
         );
-        let attr_list = LPPROC_THREAD_ATTRIBUTE_LIST(HeapAlloc(
-            GetProcessHeap().unwrap(),
-            HEAP_ZERO_MEMORY,
-            attr_size,
-        ));
 
-        let _ = InitializeProcThreadAttributeList(attr_list, 1, 0, &mut attr_size);
+        let attr_list = LPPROC_THREAD_ATTRIBUTE_LIST(
+            HeapAlloc(GetProcessHeap()?, HEAP_ZERO_MEMORY, attr_size)
+        );
 
-        let _ = UpdateProcThreadAttribute(
+        InitializeProcThreadAttributeList(attr_list, 1, 0, &mut attr_size)?;
+
+        // Update the attribute list with the parent process handle
+        UpdateProcThreadAttribute(
             attr_list,
             0,
             PROC_THREAD_ATTRIBUTE_PARENT_PROCESS as usize,
@@ -44,23 +48,27 @@ fn main() {
             size_of::<HANDLE>(),
             None,
             None,
-        );
+        )?;
 
-        let windir = std::env::var("WINDIR").unwrap() + "\\System32\\notepad.exe";
-        startup_info.lpAttributeList = attr_list;
-        let _ = CreateProcessA(
+        // Create the new process (Notepad) with spoofed parent
+        let windir = std::env::var("WINDIR").unwrap_or("".to_string()) + "\\System32\\notepad.exe";
+        si.lpAttributeList = attr_list;
+        CreateProcessA(
             None,
-            PSTR(windir.as_ptr() as _),
+            PSTR(windir.as_ptr().cast_mut()),
             None,
             None,
             false,
             EXTENDED_STARTUPINFO_PRESENT,
             None,
             None,
-            &startup_info.StartupInfo,
-            &mut process_info,
-        );
+            &si.StartupInfo,
+            &mut pi,
+        )?;
 
+        // Free allocated memory for attribute list
         DeleteProcThreadAttributeList(attr_list);
     }
+
+    Ok(())
 }
