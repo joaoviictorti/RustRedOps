7bd749f17f9e4945dd8746c049df629e22974424
joaoviictorti <joaovictorti08@gmail.com>
2025-06-20 14:42:55 -0300

    Small fix

--- Execute_Command/src/main.rs
+++ Execute_Command/src/main.rs
@@ -2,24 +2,24 @@ use std::process::Command;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    #[cfg(target_os = "windows")] {
+    #[cfg(target_os = "windows")] 
+    {
         let command = Command::new("powershell")
             .arg("-c")
             .arg("whoami")
-            .output()
-            .unwrap();
+            .output()?;
         
         println!("{}", String::from_utf8_lossy(&command.stdout));
 
         Command::new("calc.exe").spawn()?;
     }
     
-    #[cfg(target_os = "linux")] {
+    #[cfg(target_os = "linux")] 
+    {
         let command = Command::new("/bin/bash")
             .arg("-c")
             .arg("id")
-            .output()
-            .unwrap();
+            .output()?;
 
         println!("{}", String::from_utf8_lossy(&command.stdout));
     }
