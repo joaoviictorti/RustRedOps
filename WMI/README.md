935b241c64fe419002a43c74404b73c83c7d4a01
joaoviictorti <joaovictorti08@gmail.com>
2025-07-14 18:38:03 -0300

    Update README.md

--- WMI/README.md
+++ WMI/README.md
@@ -1,21 +1,11 @@
 # WMI 🦀
 
-<p align="left">
-	<a href="https://www.rust-lang.org/"><img src="https://img.shields.io/badge/made%20with-Rust-red"></a>
-	<a href="#"><img src="https://img.shields.io/badge/platform-windows-blueviolet"></a>
-</p>
-
-- [Overview](#overview)
-- [Usage](#usage)
-
-# Overview
+## Overview
 Simple WMI query demo using Rust
 
-# Usage 
+## Usage 
+
 You can run with cargo run or the compiled binary directly:
 ```sh
-cargo run
-```
-```sh
-target/release/wmi_exec.exe
+cargo run --release
 ```
--- WMI/src/main.rs
+++ WMI/src/main.rs
@@ -4,7 +4,7 @@ use wmi::{COMLibrary, Variant, WMIConnection};
 fn main() -> Result<(), wmi::WMIError> {
     let _com_library = COMLibrary::new()?;
     let wmi_connection = unsafe { WMIConnection::with_initialized_com(Some("root\\SecurityCenter2"))? };
-    let avs: Vec<HashMap<String, Variant>> = wmi_connection.raw_query("SELECT * FROM AntiVirusProduct")?;
+    let avs = wmi_connection.raw_query::<HashMap<String, Variant>>("SELECT * FROM AntiVirusProduct")?;
     for result in avs {
         println!("Infos AntivirusProduct:");
         println!("{:#?}", result);
